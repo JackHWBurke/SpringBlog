c1af66eaf08be337435b75e7970bde268ece02d5
rossellemacabata <rossellemacabata@gmail.com>
2018-01-24 16:21:30 -0800

    Created PostService interface and implemented PostService methods in PostServiceStubImpl, also changed HomeController

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -1,7 +1,14 @@
 package blog.controllers;
 
+import blog.models.Post;
+import blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 // defines a spring web mvc controller
@@ -9,8 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class HomeController {
+    // autowired automatically injects the correct implementation for the services
+    // Spring scans the project and finds all classes that implement the service interface
+    @Autowired
+
+    private PostService postService;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        List<Post> latest5Posts = postService.findLatest5();
+        model.addAttribute("latest5posts", latest5Posts);
+
+        List<Post> latest3Posts = latest5Posts.stream()
+                .limit(3).collect(Collectors.toList());
+        model.addAttribute("latest3posts", latest3Posts);
+
+
         return "index";
     }
 }
---
+++ src/main/java/blog/services/PostService.java
@@ -0,0 +1,17 @@
+package blog.services;
+
+import blog.models.Post;
+
+import java.util.List;
+
+// interface provides all functionality about posts
+
+public interface PostService {
+    List<Post> findAll();
+    List<Post> findLatest5();
+    Post findById(Long id);
+    Post create(Post post);
+    Post edit(Post post);
+    void deleteById(Long id);
+
+}
